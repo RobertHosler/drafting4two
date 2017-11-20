8367e9d96f0f8fee1ffa19c1916846f8905a9315
Roqb Hosler <rph731@gmail.com>
2017-11-20 02:59:19 +0000

    Pancake module mirroring winston

--- web/js/pancake.js
+++ web/js/pancake.js
@@ -1,3 +1,7 @@
+/* global $*/
+/* global mtg*/
+/* global draft*/
+
 /*
 State consists of 2 packs passed between two players.
 
@@ -19,19 +23,88 @@ Burn 4
  * Pancake draft is a draft format module that will be initialized and passed into a draft module.
  */
 var pancake = (function() {
-
+    
     var format = "pancake";
-    //TODO: create state method
+    var state;
+    var isDraftComplete = false;
+
     
     var makePick = function(cardName) {
-        //TODO: process a card being chosen for a deck  
+        //TODO: process a card being chosen for a deck
+        if (!draft.instanse) {
+			draft.instanse = true;
+			$.ajax({
+				type: "POST",
+				url: "draft_winston.php",
+				data: {
+					'function': 'makePick',
+					'draftName': draftName,
+					'playerNumber': playerNumber
+				},
+				dataType: "json",
+				success: function(data) {
+					this.state = data.state;
+					processDataChange(data.state);
+					draft.processDataChange(data.state);
+					draft.instanse = false;
+				},
+			});
+		}
+		else {
+			setTimeout(makePick, 100);
+		}
     };
     
     var burnPick = function(cardName) {
         //TODO: process removing a card from the pack
+        if (!draft.instanse) {
+			draft.instanse = true;
+			$.ajax({
+				type: "POST",
+				url: "draft_winston.php",
+				data: {
+					'function': 'burnPick',
+					'draftName': draftName,
+					'playerNumber': playerNumber
+				},
+				dataType: "json",
+				success: function(data) {
+					this.state = data.state;
+					processDataChange(data.state);
+					draft.processDataChange(data.state);
+					draft.instanse = false;
+				},
+			});
+		}
+		else {
+			setTimeout(burnPick, 100);
+		}
     };
+	
+	var isStateUpdated = function(_state) {
+		return this.state.players.length != _state.players.length;
+	};
+    
+	var	processDataChange = function(state) {
+		if (state.piles) {
+			updateAllPiles(state);
+			var isActivePlayer = (state.activePlayer == playerNumber);
+			updateActivePlayer(state, isActivePlayer);
+		}
+		else {
+			isDraftComplete = true;
+			$("#draftComplete").show();
+			$("#topButtons").hide();
+			$("#buttonRow").hide();
+			$("#currentPileRow").hide();
+		}
+	};
 
 	return {
-	    
+		processDataChange: processDataChange,
+		isDraftComplete: isDraftComplete,
+		isStateUpdated: isStateUpdated,
+		burnPick: burnPick,
+		makePick: makePick
 	};
 })();
