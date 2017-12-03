b7a49cb1e33d52a0a4fd675e3200a9b23aa4c3bf
Roqb Hosler <rph731@gmail.com>
2017-12-03 05:59:51 +0000

    Device width

--- web/d42/login.php
+++ web/d42/login.php
@@ -15,6 +15,7 @@
 	
 	<head>
 	    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
 	    <title>Login</title>
 	    <link rel="stylesheet" text="text/css" href="/libs/bootstrap/3.3.5/bootstrap.min.css"/>
 	    <link rel="stylesheet" text="text/css" href="/libs/bootstrap/3.3.5/bootstrap-theme.min.css"/>
